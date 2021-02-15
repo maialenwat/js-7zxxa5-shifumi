213197de571ad28880a92a97ebd33039c3fffb24
maialenwat <maialen.watrigant@gmail.com>
2021-02-15 17:54:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-7zxxa5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-7zxxa5)
---
+++ index.js
@@ -0,0 +1,12 @@
+// Import stylesheets
+import "./style.css";
+
+// Write Javascript code!
+const appDiv = document.getElementById("app");
+appDiv.innerHTML = `<h1>JS Starter Shifumi</h1>`;
+
+/**
+ * 1ère étape: récupérer les valeurs
+ * 2ème étape: comparer les valeurs
+ * 3ème étape: afficher les valeurs
+ */
